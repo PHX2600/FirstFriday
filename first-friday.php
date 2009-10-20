9ee0396c6912045bf38c019bb2183abf3d71a3ba
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-10-19 18:56:59 -0700

    Converted script to function an increased version to 0.2

--- first-friday.php
+++ first-friday.php
@@ -1,26 +1,35 @@
-<?php // first-friday.php v0.1.4 by, Chris Kankiewicz (http://www.web-geek.net)
+<?php
+	echo(firstFriday(true));
 
-  // Calculate next Friday timestamp
-  for ($x = date('d'); $x <= (date('d') + 6); $x++) {
-    $timeStamp = mktime(0,0,0,date('m'),$x,date('Y'));
-    if (date('w',$timeStamp) == 5) {
-      $nextFriday = mktime(0,0,0,date('m'),$x,date('Y'));
-    }
-  }
-  // Check if next Friday is the first friday of the month.
-  if (date('d', $nextFriday) <= 7) {
-    $firstFriday = $nextFriday;
-  } else {
-  // Calculate first Friday of next month
-    for ($x = 1; $x <= 7; $x++) {
-      $timeStamp = mktime(0,0,0,date('m')+1,$x,date('Y'));
-      if (date('w',$timeStamp) == 5) {
-        $firstFriday = mktime(0,0,0,date('m')+1,$x,date('Y'));
-      }
-    }
-  }
-
-  // Echo next first Friday
-  echo $firstFriday;
-
+	function firstFriday($formatted = false) {
+		// firstFriday v0.2 by, Chris Kankiewicz (http://www.ChrisKankiewicz.com)
+		 
+		// Calculate next Friday timestamp
+		for ($x = date('d'); $x <= (date('d') + 6); $x++) {
+			$timeStamp = mktime(0,0,0,date('m'),$x,date('Y'));
+			if (date('w',$timeStamp) == 5) {
+				$nextFriday = mktime(0,0,0,date('m'),$x,date('Y'));
+			}
+		}
+		// Check if next Friday is the first friday of the month.
+		if (date('d', $nextFriday) <= 7) {
+			$firstFriday = $nextFriday;
+		} else {
+			// Calculate first Friday of next month
+			for ($x = 1; $x <= 7; $x++) {
+				$timeStamp = mktime(0,0,0,date('m')+1,$x,date('Y'));
+				if (date('w',$timeStamp) == 5) {
+					$firstFriday = mktime(0,0,0,date('m')+1,$x,date('Y'));
+				}
+			}
+		}
+		 
+		if ($formatted) {
+			// Return date as "Month Day, Year" format
+			return date("F j, Y", $firstFriday);
+		} else {
+			// Return next first Friday timestamp
+			return $firstFriday;
+		}
+	}
 ?>
